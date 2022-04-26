5e182d0632cce957aaeda1ad7ecaa89c1f01a5f0
Adriana Venegas <adrianne.venegas@gmail.com>
2022-04-26 16:24:06 -0500

    Sets y Gets - estructurar código

--- clases-ESNext/assets/js/classes/class.js
+++ clases-ESNext/assets/js/classes/class.js
@@ -1,15 +1,29 @@
-
+// Orden, cómo estructurar el código
+// 1 - Propiedades y métodos estáticos
+// 2 - Propiedades de la clase
+// 3 - Constructor
+// 4 - Sets y Gets
+// 5 - Métodos
+// 6- Todo lo privado al final de la clase
 
 class Persona {
     nombre = '';
     codigo = '';
     frase  = '';
+    comida = '';
 
     constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
         
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase  = frase;
+    }
+
+    set setComidaFavorita( comida ) {
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita() {
+        return `La comida favorita de ${ this.nombre } es ${ this.comida }`;
     }
 
     quienSoy() {
@@ -28,7 +42,10 @@ class Persona {
 const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman' );
 const ironman   = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman' );
 console.log( spiderman );
-console.log( ironman );
-spiderman.miFrase();
-ironman.miFrase();
+// console.log( ironman );
+// spiderman.miFrase();
+// ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+
 
