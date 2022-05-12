8896d9b8d9b7839ed38a5650d46bb6a93a1e1e93
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-12 13:52:10 -0500

    Construir tareas en el html

--- TODO/src/index.js
+++ TODO/src/index.js
@@ -1,14 +1,15 @@
 import './styles.css';
 
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
 export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JS');
-const tarea2 = new Todo('Almorzar');
+const tarea = new Todo('Aprender JS!!');
 
 todoList.nuevoTodo( tarea );
-todoList.nuevoTodo( tarea2 );
 
 console.log(todoList);
+
+crearTodoHtml( tarea );
