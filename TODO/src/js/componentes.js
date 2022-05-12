a603418f3cc73d9e5d54914ad8a13e5b186e1547
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-12 18:25:18 -0500

    Aplicar filtros

--- TODO/src/js/componentes.js
+++ TODO/src/js/componentes.js
@@ -86,3 +86,38 @@ btnBorrar.addEventListener('click', () => {
 });
 
 
+ulFiltros.addEventListener('click', (event) => {
+
+    const filtro = event.target.text;
+    if (!filtro) { return; }
+
+    anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+
+        }
+
+
+    }
+
+
+
+});
