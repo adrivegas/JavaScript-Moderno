d7e40706e0f93fa30430ece4c9a692a9df1991e8
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-19 17:12:36 -0500

    Promise.race

--- promesas-callbacks/src/index.js
+++ promesas-callbacks/src/index.js
@@ -1,3 +1,16 @@
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas'
 
-import './styles.css';
+// promesaLenta.then( console.log );
+// promesaMedia.then( console.log );
+// promesaRapida.then( console.log );
 
+// Promise.race ejecuta el conjunto de promesas y obtiene el mensaje
+// de la promesa que se resuelve más rápido
+Promise.race([ promesaLenta, promesaMedia, promesaRapida])
+    .then(console.log)
+    .catch(console.warn);
+
+// En el caso que falle la promesa rápida(reject) 
+// vamos a recibir el error en amarillo por ser la primera que se resuelve
+// por el contrario si fallan: promesaLenta, promesaMedia, no 
+// recibimos error sino el console.log de la promesaRapida
