c79dce19ea503c51d4ce392fb67c1fd288d9ca0a
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-24 17:03:21 -0500

    Crear una tabla con los usuarios

--- HTTP/src/index.js
+++ HTTP/src/index.js
@@ -1,5 +1,4 @@
 // import { init } from "./js/chistes-page";
-import { obtenerUsuarios } from "./js/http-provider";
+import { init } from "./js/usuarios-page";
 
-// init();
-obtenerUsuarios().then(console.log);
+init();
