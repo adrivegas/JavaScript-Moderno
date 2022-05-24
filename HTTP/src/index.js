d58aa802eec655bb7d5e8c97876932e2ad5e1270
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-24 14:21:00 -0500

    centralizar las peticiones HTTP

--- HTTP/src/index.js
+++ HTTP/src/index.js
@@ -1,27 +1,3 @@
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+import { obtenerChiste } from "./js/http-provider";
 
-fetch(jokeUrl).then(resp => {
-    console.log(resp);
-    // le decimos a la respuesta que extraiga el body en formato de json
-    resp.json().then(data => {
-        console.log(data);
-    });
-});
-
-// Lo anterior usando desestructuración, para obtener los valores que necesito
-
-fetch(jokeUrl).then(resp => {
-    // le decimos a la respuesta que extraiga el body en formato de json
-    resp.json().then(({ id, value}) => {
-        console.log(id);
-        console.log(value);
-    });
-});
-
-// Mejor forma de ejecutar el código anterior
-
-fetch(jokeUrl)
-    .then( resp => resp.json())
-    .then( ({ id, value}) => {
-        console.log(id, value);
-    });
+obtenerChiste().then(console.log);
