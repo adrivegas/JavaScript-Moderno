dd150fc600b00bb98b3d789f611584dd1c649639
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-24 16:53:35 -0500

    Obtener lista de usuarios

--- HTTP/src/index.js
+++ HTTP/src/index.js
@@ -1,3 +1,5 @@
-import { init } from "./js/chistes-page";
+// import { init } from "./js/chistes-page";
+import { obtenerUsuarios } from "./js/http-provider";
 
-init();
+// init();
+obtenerUsuarios().then(console.log);
