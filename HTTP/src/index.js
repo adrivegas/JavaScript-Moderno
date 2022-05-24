e18af961e5612070e17130d94ab5fd7ccc1368b6
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-24 13:54:41 -0500

    Promesas en cadena

--- HTTP/src/index.js
+++ HTTP/src/index.js
@@ -11,10 +11,17 @@ fetch(jokeUrl).then(resp => {
 // Lo anterior usando desestructuración, para obtener los valores que necesito
 
 fetch(jokeUrl).then(resp => {
-    console.log(resp);
     // le decimos a la respuesta que extraiga el body en formato de json
     resp.json().then(({ id, value}) => {
         console.log(id);
         console.log(value);
     });
 });
+
+// Mejor forma de ejecutar el código anterior
+
+fetch(jokeUrl)
+    .then( resp => resp.json())
+    .then( ({ id, value}) => {
+        console.log(id, value);
+    });
