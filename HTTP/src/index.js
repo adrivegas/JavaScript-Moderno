1cb61d63bfaef2ef57a9a81b5ee6cd4ba437d5cb
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-24 13:48:46 -0500

    Fetch -get

---
+++ HTTP/src/index.js
@@ -0,0 +1,20 @@
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+fetch(jokeUrl).then(resp => {
+    console.log(resp);
+    // le decimos a la respuesta que extraiga el body en formato de json
+    resp.json().then(data => {
+        console.log(data);
+    });
+});
+
+// Lo anterior usando desestructuración, para obtener los valores que necesito
+
+fetch(jokeUrl).then(resp => {
+    console.log(resp);
+    // le decimos a la respuesta que extraiga el body en formato de json
+    resp.json().then(({ id, value}) => {
+        console.log(id);
+        console.log(value);
+    });
+});
