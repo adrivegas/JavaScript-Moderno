464c08a22f5aafafc98e887a690548378a424b1c
Adriana Venegas <adrianne.venegas@gmail.com>
2022-05-24 19:42:39 -0500

    Crear html para subida de archivos

--- HTTP/src/index.js
+++ HTTP/src/index.js
@@ -1,17 +1,21 @@
 // import { init } from "./js/chistes-page";
 // import { init } from "./js/usuarios-page";
 
-import * as CRUD from "./js/crud-provider";
+// import * as CRUD from "./js/crud-provider";
 
-CRUD.getUsuario(2).then(console.log);
-CRUD.crearUsuario({
-    name: 'Adriana',
-    job: 'Frontend'
-}).then(console.log);
+// CRUD.getUsuario(2).then(console.log);
+// CRUD.crearUsuario({
+//     name: 'Adriana',
+//     job: 'Frontend'
+// }).then(console.log);
 
-CRUD.actualizarUsuario(1, {
-    name: 'Violeta',
-    job: 'Pintora'
-}).then(console.log);
+// CRUD.actualizarUsuario(1, {
+//     name: 'Violeta',
+//     job: 'Pintora'
+// }).then(console.log);
 
-CRUD.borrarUsuario( 1 ).then( console.log );
+// CRUD.borrarUsuario( 1 ).then( console.log );
+
+import { init } from "./js/archivos-page";
+
+init();
